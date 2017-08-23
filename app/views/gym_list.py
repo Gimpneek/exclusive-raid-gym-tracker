d5e1340323870ae0938c36b5b1275c3769e6f93d
Gimpneek <colin@gimpneek.com>
2017-08-23 23:52:55 +0100

    Changing gym list sort order

--- app/views/gym_list.py
+++ app/views/gym_list.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from app.models.gym_item import GymItem
 from app.models.profile import Profile
+from django.db.models import F
 
 
 @login_required
@@ -11,7 +12,7 @@ def gym_list(request):
     Show the homepage
     """
     profile = Profile.objects.get(user=request.user.id)
-    gym_list = GymItem.objects.filter(profile=profile.id, hidden=False).order_by('last_visit_date', 'gym__name')
+    gym_list = GymItem.objects.filter(profile=profile.id, hidden=False).order_by(F('last_visit_date').asc(nulls_first=True), 'gym__name')
     total_gyms = len(gym_list)
     completed_gyms = len([g for g in gym_list if g.last_visit_date])
     return render(request, 'app/gym_list.html', {
