44d2c4249feca061a4b9b5024e307fcc17c3993e
Gimpneek <colin@gimpneek.com>
2017-08-23 23:17:00 +0100

    Cleaning up design - even more

--- app/views/gym_list.py
+++ app/views/gym_list.py
@@ -12,6 +12,11 @@ def gym_list(request):
     """
     profile = Profile.objects.get(user=request.user.id)
     gym_list = GymItem.objects.filter(profile=profile.id, hidden=False).order_by('last_visit_date', 'gym__name')
+    total_gyms = len(gym_list)
+    completed_gyms = len([g for g in gym_list if g.last_visit_date])
     return render(request, 'app/gym_list.html', {
-        'gym_list': gym_list
+        'gym_list': gym_list,
+        'total_gyms': total_gyms,
+        'completed_gyms': completed_gyms,
+        'gym_progress': int((float(completed_gyms)/float(total_gyms))*100)
     })
