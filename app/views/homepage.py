206892ad41e91dddc0d36a9065bb413f4cfea65b
Gimpneek <colin@gimpneek.com>
2017-08-24 18:52:11 +0100

    - Error fixing

--- app/views/homepage.py
+++ app/views/homepage.py
@@ -6,6 +6,6 @@ def index(request):
     """
     Show the homepage
     """
-    if request.user:
+    if request.user.id:
         return redirect('gym_list')
     return render(request, 'app/homepage.html')
