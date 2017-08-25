33144c51b65766a8697eb1abb4ec45af1df01f94
Gimpneek <colin@gimpneek.com>
2017-08-25 21:52:04 +0100

    Adding reset button

--- exclusive_raid_tracker/urls.py
+++ exclusive_raid_tracker/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
 from app.views.homepage import index
 from app.views.login import login_page
 from app.views.logout import logout_page
@@ -22,6 +21,7 @@ from app.views.gym_list import gym_list
 from app.views.signup import signup_page
 from app.views.gym_item import gym_item
 from app.views.hide_gym_item import hide_gym_item
+from app.views.reset_gym_item import reset_gym_item
 
 urlpatterns = [
     url(r'^$', index, name='index'),
@@ -30,5 +30,6 @@ urlpatterns = [
     url(r'^signup/$', signup_page, name='signup'),
     url(r'^list/$', gym_list, name='gym_list'),
     url(r'^item/(?P<gym_item_id>\d+)/$', gym_item, name='gym_item'),
-    url(r'^hide/(?P<gym_item_id>\d+)/$', hide_gym_item, name='hide_gym_item')
+    url(r'^hide/(?P<gym_item_id>\d+)/$', hide_gym_item, name='hide_gym_item'),
+    url(r'^reset/(?P<gym_item_id>\d+)/$', reset_gym_item, name='reset_gym_item'),
 ]
