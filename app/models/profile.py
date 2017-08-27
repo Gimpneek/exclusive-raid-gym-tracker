ff9f673306748ab6725e40263924164b8e49fe9c
Gimpneek <colin@gimpneek.com>
2017-08-27 20:22:11 +0100

    - Using user's username for object representation

--- app/models/profile.py
+++ app/models/profile.py
@@ -7,4 +7,7 @@ class Profile(models.Model):
     pokemon_go_username = models.CharField(max_length=128, help_text="Your name in Pokemon Go")
 
     def __unicode__(self):
-        return self.pokemon_go_username
+        return self.user.username
+
+    def __str__(self):
+        return self.user.username
