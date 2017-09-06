50f92c5895d2daaaff06a67feb31a8b099fc301b
Gimpneek <colin@gimpneek.com>
2017-09-06 23:03:26 +0100

    - Adding raid fields to store raid pokemon, raid level and raid end date

--- app/tests/models/test_gym.py
+++ app/tests/models/test_gym.py
@@ -8,6 +8,9 @@ GYM_NAME = 'Test Gym'
 GYM_LOCATION = 'this way, that way'
 GYM_IMAGE = 'http://www.test.com/image.png'
 GYM_ID = '0123456789'
+RAID_POKEMON = 'Mewtwo'
+RAID_LEVEL = 5
+RAID_END_DATE = '1988-01-12 06:00:00+0000'
 
 
 class TestGymObject(TestCase):
@@ -22,7 +25,10 @@ class TestGymObject(TestCase):
             name=GYM_NAME,
             location=GYM_LOCATION,
             image_url=GYM_IMAGE,
-            gym_hunter_id=GYM_ID
+            gym_hunter_id=GYM_ID,
+            raid_pokemon=RAID_POKEMON,
+            raid_level=RAID_LEVEL,
+            raid_end_date=RAID_END_DATE
         )
 
     def test_gym_name(self):
@@ -44,6 +50,18 @@ class TestGymObject(TestCase):
     def test_object_string(self):
         """ Test the string the object returns """
         self.assertEqual(self.gym.__str__(), GYM_NAME)
+
+    def test_raid_pokemon(self):
+        """ Test the Raid Pokemon name on the created gym object """
+        self.assertEqual(self.gym.raid_pokemon, RAID_POKEMON)
+
+    def test_raid_level(self):
+        """ Test the raid level on the created gym object """
+        self.assertEqual(self.gym.raid_level, RAID_LEVEL)
+
+    def test_raid_end_date(self):
+        """ Test the raid level end date on the created gym object """
+        self.assertEqual(self.gym.raid_end_date, RAID_END_DATE)
 
 
 class TestGymObjectCreation(TestCase):
@@ -88,5 +106,50 @@ class TestGymObjectCreation(TestCase):
             name=GYM_NAME,
             location=GYM_LOCATION,
             gym_hunter_id=GYM_ID
+        )
+        gym.full_clean()
+
+    @staticmethod
+    def test_raid_pokemon_not_defined():
+        """
+        Test that exception is not raised if no raid pokemon is supplied
+        """
+        gym = Gym.objects.create(
+            name=GYM_NAME,
+            location=GYM_LOCATION,
+            gym_hunter_id=GYM_ID,
+            image_url=GYM_IMAGE,
+            raid_level=RAID_LEVEL,
+            raid_end_date=RAID_END_DATE
+        )
+        gym.full_clean()
+
+    @staticmethod
+    def test_raid_level_not_defined():
+        """
+        Test that exception is not raids if no raid level is supplied
+        """
+        gym = Gym.objects.create(
+            name=GYM_NAME,
+            location=GYM_LOCATION,
+            gym_hunter_id=GYM_ID,
+            image_url=GYM_IMAGE,
+            raid_pokemon=RAID_POKEMON,
+            raid_end_date=RAID_END_DATE
+        )
+        gym.full_clean()
+
+    @staticmethod
+    def test_raid_end_not_defined():
+        """
+        Test that exception is not raised if no raid end date is suppled
+        """
+        gym = Gym.objects.create(
+            name=GYM_NAME,
+            location=GYM_LOCATION,
+            gym_hunter_id=GYM_ID,
+            image_url=GYM_IMAGE,
+            raid_level=RAID_LEVEL,
+            raid_pokemon=RAID_POKEMON
         )
         gym.full_clean()
