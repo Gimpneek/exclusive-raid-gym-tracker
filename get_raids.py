919bbc2786f9bb777c844a3a4d5128a3f05d0aa6
Gimpneek <colin@gimpneek.com>
2017-09-07 02:54:11 +0100

    - Making sure don't do anything if don't get a 200 status code back

--- get_raids.py
+++ get_raids.py
@@ -24,18 +24,19 @@ params = {
 }
 
 raids = requests.get(os.environ.get('POGO_MAP_URL'), params=params)
-gym_data = raids.json().get('gyms', {})
+if raids.status_code == 200:
+    gym_data = raids.json().get('gyms', {})
 
-for gym_id, status in gym_data.items():
-    raid_end = datetime.fromtimestamp(
-        (status.get('raid_end_ms')/1000.0),
-        tz=pytz.UTC
-    )
-    time_left = raid_end - datetime.now(tz=pytz.utc)
-    if time_left.total_seconds() > 0:
-        gym = Gym.objects.get(gym_hunter_id=gym_id)
-        if gym:
-            gym.raid_level = status.get('raid_level')
-            gym.raid_pokemon = status.get('raid_pokemon_name')
-            gym.raid_end_date = raid_end
-            gym.save()
+    for gym_id, status in gym_data.items():
+        raid_end = datetime.fromtimestamp(
+            (status.get('raid_end_ms')/1000.0),
+            tz=pytz.UTC
+        )
+        time_left = raid_end - datetime.now(tz=pytz.utc)
+        if time_left.total_seconds() > 0:
+            gym = Gym.objects.get(gym_hunter_id=gym_id)
+            if gym:
+                gym.raid_level = status.get('raid_level')
+                gym.raid_pokemon = status.get('raid_pokemon_name')
+                gym.raid_end_date = raid_end
+                gym.save()
