c27f9704a566d28ddfedf2093e2521076df067b9
Gimpneek <colin@gimpneek.com>
2017-09-23 22:54:20 +0100

    - Github repo name was changed so need to update

--- exclusive_raid_tracker/settings.py
+++ exclusive_raid_tracker/settings.py
@@ -134,6 +134,7 @@ if os.environ.get('PROD', '0') == '1':
     # Update database configuration with $DATABASE_URL.
     DB_FROM_ENV = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(DB_FROM_ENV)
+    SECURE_SSL_REDIRECT = True
     MIDDLEWARE += [
         'raven.contrib.django.raven_compat.middleware'
         '.SentryResponseErrorIdMiddleware',
