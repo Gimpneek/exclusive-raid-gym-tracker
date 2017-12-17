7eeec17f89e6b61eff1c66b7b9e069e9a24a413c
Gimpneek <colin@gimpneek.com>
2017-12-17 23:34:11 +0000

    - Starting to move raids script inline with upstream API changes

--- get_raids.py
+++ get_raids.py
@@ -9,6 +9,13 @@ django.setup()
 from app.models.gym import Gym
 from app.models.raid_item import RaidItem
 
+new_params = {
+    'type': 'raids',
+    'area': 'leeds',
+    'fields': 'all',
+    'api-key': os.environ.get('GO_MAPS_KEY')
+}
+
 params = {
     'by': 'leeds',
     'pokemon': 'false',
@@ -24,54 +31,54 @@ params = {
     'raids': 'false'
 }
 
-proxy_url = os.environ.get('QUOTAGUARDSTATIC_URL')
+# proxy_url = os.environ.get('QUOTAGUARDSTATIC_URL')
+#
+# time_now = datetime.now(tz=pytz.timezone('Europe/London'))
+#
+# if time_now.hour in range(6, 21):
+scraper = cfscrape.create_scraper()
+#     if proxy_url:
+#         proxies = {
+#             "http": proxy_url,
+#             "https": proxy_url
+#         }
+#         raids = scraper.get(
+#             os.environ.get('POGO_MAP_URL'),
+#             params=params,
+#             proxies=proxies
+#         )
+#     else:
+raids = scraper.post(os.environ.get('POGO_MAP_URL'), data=new_params)
 
-time_now = datetime.now(tz=pytz.timezone('Europe/London'))
+if raids.status_code == 200:
+    gym_data = raids.json().get('raids', {})
 
-if time_now.hour in range(6, 21):
-    scraper = cfscrape.create_scraper()
-    if proxy_url:
-        proxies = {
-            "http": proxy_url,
-            "https": proxy_url
-        }
-        raids = scraper.get(
-            os.environ.get('POGO_MAP_URL'),
-            params=params,
-            proxies=proxies
-        )
-    else:
-        raids = scraper.get(os.environ.get('POGO_MAP_URL'), params=params)
-
-    if raids.status_code == 200:
-        gym_data = raids.json().get('gyms', {})
-
-        for status in gym_data:
-            if status.get('raid_end_ms') and status.get('raid_level'):
-                raid_end = datetime.fromtimestamp(
-                    (status.get('raid_end_ms')/1000.0),
-                    tz=pytz.UTC
-                )
-                now = datetime.now(tz=pytz.utc)
-                time_left = raid_end - now
-                if time_left.total_seconds() > 0:
-                    try:
-                        gym = Gym.objects.get(name=status.get('gym_name'))
-                    except Gym.DoesNotExist:
-                        gym = None
-                    if gym:
-                        raids = RaidItem.objects.filter(
+    for status in gym_data:
+        if status.get('raid_end_ms') and status.get('raid_level'):
+            raid_end = datetime.fromtimestamp(
+                (status.get('raid_end_ms')/1000.0),
+                tz=pytz.UTC
+            )
+            now = datetime.now(tz=pytz.utc)
+            time_left = raid_end - now
+            if time_left.total_seconds() > 0:
+                try:
+                    gym = Gym.objects.get(name=status.get('gym_name'))
+                except Gym.DoesNotExist:
+                    gym = None
+                if gym:
+                    raids = RaidItem.objects.filter(
+                        gym=gym,
+                        end_date=raid_end
+                    )
+                    if raids:
+                        raid = raids[0]
+                        raid.pokemon = status.get('raid_pokemon_name')
+                        raid.save()
+                    else:
+                        RaidItem.objects.create(
                             gym=gym,
+                            level=status.get('raid_level'),
+                            pokemon=status.get('raid_pokemon_name'),
                             end_date=raid_end
                         )
-                        if raids:
-                            raid = raids[0]
-                            raid.pokemon = status.get('raid_pokemon_name')
-                            raid.save()
-                        else:
-                            RaidItem.objects.create(
-                                gym=gym,
-                                level=status.get('raid_level'),
-                                pokemon=status.get('raid_pokemon_name'),
-                                end_date=raid_end
-                            )
