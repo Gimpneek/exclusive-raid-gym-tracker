da224e913db4d2c26b33a9d8bb430f41db7980b0
Gimpneek <colin@gimpneek.com>
2017-12-21 01:15:43 +0000

    - Updating tracked_gyms to tracked-gyms as underscores are bad

--- app/features/steps/page_object_models/selectors/gym_management.py
+++ app/features/steps/page_object_models/selectors/gym_management.py
@@ -1,10 +1,10 @@
 """ Selectors for gym management page """
 from selenium.webdriver.common.by import By
 
-TRACKED_GYMS_TABLE = (By.CSS_SELECTOR, '#tracked_gyms > table')
+TRACKED_GYMS_TABLE = (By.CSS_SELECTOR, '#tracked-gyms > table')
 TRACKED_GYMS_TABLE_ROW = (
     By.CSS_SELECTOR,
-    '#tracked_gyms > table > tbody > tr'
+    '#tracked-gyms > table > tbody > tr'
 )
 TABLE_COLUMNS = (By.CSS_SELECTOR, 'td')
 REMOVE_BUTTON = (By.CSS_SELECTOR, 'a')
