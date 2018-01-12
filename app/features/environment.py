0a854347e40d0591df2f4285348fdc425125c834
Gimpneek <colin@gimpneek.com>
2018-01-12 00:38:36 +0000

    - Adding print to before_feature to see is issue happens before feature

--- app/features/environment.py
+++ app/features/environment.py
@@ -1,26 +1,22 @@
 from selenium import webdriver
-import os
 
 
 def after_scenario(context, scenario):
-    print("Closing the browser now")
     context.browser.close()
-
-
-# def before_feature(context, feature):
-#     context.fixtures = ['gym-fixtures.json', 'test-fixtures.json']
 
 
 def before_all(context):
     context.fixtures = ['gym-fixtures.json', 'test-fixtures.json']
 
 
-def after_feature(context, feature):
-    print("After the feature")
+def before_feature(context, feature):
+    print("Before the feature")
 
 
 def before_scenario(context, scenario):
+    print("about to create browser")
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     context.browser = webdriver.Chrome(chrome_options=options)
+    print("Created browser")
