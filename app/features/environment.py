ea921907432e31199f8e3dc0f1a2c3e2623cb46f
Gimpneek <colin@gimpneek.com>
2018-01-12 01:10:33 +0000

    - Seeing if using deprecated image helps

--- app/features/environment.py
+++ app/features/environment.py
@@ -9,14 +9,8 @@ def before_all(context):
     context.fixtures = ['gym-fixtures.json', 'test-fixtures.json']
 
 
-def before_feature(context, feature):
-    print("Before the feature")
-
-
 def before_scenario(context, scenario):
-    print("about to create browser")
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     context.browser = webdriver.Chrome(chrome_options=options)
-    print("Created browser")
